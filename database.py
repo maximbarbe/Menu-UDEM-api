021709135ecfa8ce4ed8a0d9a491193f6540a744
MaximBarbe <barbe.maxim@gmail.com>
2023-05-12 22:41:27 -0400

    work on db

--- database.py
+++ database.py
@@ -2,4 +2,32 @@
 
 import sqlite3
 
+connection = sqlite3.connect("data.db")
+c = connection.cursor()
+# user_creation = """
+#         CREATE TABLE users
+#         (ID         INTEGER         PRIMARY KEY,
+#         USERNAME    CHAR(20)        NOT NULL,
+#         EMAIL       CHAR(100)       NOT NULL,
+#         PASSWORD    CHAR(20)        NOT NULL,
+#         ROLE        TEXT            NOT NULL)
 
+
+# """
+
+
+# FOREIGN KEY(COMMANDE_ID) REFERENCES COMMANDE(ID)  
+
+item = """
+        CREATE TABLE item
+        (ID             INTEGER           PRIMARY KEY,
+        
+        
+        
+        
+        )
+"""
+
+
+c.execute(user_creation)
+connection.commit()
