82814cb5eed7604e5d472f959093f8206ded603c
MaximBarbe <barbe.maxim@gmail.com>
2023-05-13 13:01:24 -0400

    Start page

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import werkzeug.security
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # Create the app
 app = FastAPI()
 
+# Fichiers HTML et CSS
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Index page
 @app.get("/", response_class=HTMLResponse)
