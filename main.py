d58ee2043218e225c9e74d35344e2664b7b1d98b
MaximBarbe <barbe.maxim@gmail.com>
2023-05-13 18:01:19 -0400

    Work on design

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import re
 import werkzeug.security
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordRequestFormStrict
 
+
+
+##################################### FASTAPI SETUP ##################################################
 # Create the app
 app = FastAPI()
 
 # Fichiers HTML et CSS
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+########################################################################################################
 
+
+
+
+
+
+
+
+###################################### ROUTES ###############################################################
 # Index page
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login")
+def get_login_page():
+    return {"Hello": "World"}
+
+@app.post("/login")
+def login(user_data: OAuth2PasswordRequestForm = Depends()):
+    print(user_data)
+    pass
+##############################################################################################################
