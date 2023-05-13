8d5bc6950fb687ebaffa62fad1c2191371e000b2
MaximBarbe <barbe.maxim@gmail.com>
2023-05-13 12:38:13 -0400

    Working on html

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import re
 import werkzeug.security
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 # Create the app
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 # Index page
-@app.get("/")
-def index():
-    return "<p>Hello World</p>"
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
