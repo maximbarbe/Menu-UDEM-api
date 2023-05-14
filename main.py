0c79026898a55087ab83b55ae6934214d7de0551
MaximBarbe <barbe.maxim@gmail.com>
2023-05-14 14:18:30 -0400

    register page changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordRequestFormStrict
+from fastapi_login import LoginManager
 
 
 
@@ -20,10 +21,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 
-
-
-
-
 ###################################### ROUTES ###############################################################
 # Index page
 @app.get("/", response_class=HTMLResponse)
@@ -33,6 +30,11 @@ def index(request: Request):
 @app.get("/login")
 def get_login_page():
     return {"Hello": "World"}
+
+@app.get("/register", response_class=HTMLResponse)
+def get_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
 
 @app.post("/login")
 def login(user_data: OAuth2PasswordRequestForm = Depends()):
