27ec0be7093a9b9ec35ce4c321f6d094f76b5e4b
MaximBarbe <barbe.maxim@gmail.com>
2023-05-18 21:17:27 -0400

    register page error handling

--- main.py
+++ main.py
@@ -62,21 +62,35 @@ async def get_login_page():
 
 @app.get("/register", response_class=HTMLResponse)
 async def get_register_page(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request, 'errors': None, 'form_data': None}, )
+    register_errors = {
+        "username": None,
+        "password_not_match": None,
+        "password_not_fit": None
+    }
+    return templates.TemplateResponse("register.html", {"request": request, 'errors': register_errors, 'form_data': None}, )
 
 @app.post("/register", response_class=HTMLResponse)
 async def post_register_page(request: Request, form_data: RegisterForm = Depends()):
     query="SELECT * FROM users WHERE EMAIL = ?"
     res = db_cursor.execute(query, (form_data.email.lower(),))
+    register_errors = {
+        "email": None,
+        "username": None,
+        "password_not_match": None,
+        "password_not_fit": None
+    }
     if res.fetchall() != []:
-        return templates.TemplateResponse("register.html", {"request": request, 'errors': 'Email déjà existant, veuillez réessayer.', 'form_data': form_data})
+        register_errors["email"] = "Email déjà existant, veuillez réssayer."
     else:
         if len(form_data.username) < 6:
-            return templates.TemplateResponse("register.html", {"request": request, 'errors': "Nom d'utilisateur trop court. Il devrait contenir au moins 6 caractères.", 'form_data': form_data})
+            register_errors["username"] = "Nom d'utilisateur trop court. Il devrait contenir au moins 6 caractères."
         if form_data.password != form_data.confirmed_password:
-            return templates.TemplateResponse('register.html', {"request": request, 'errors': 'Les mots de passes ne correspondent pas.', 'form_data': form_data})
+            register_errors["password_not_match"] = "Les mots de passes ne correspondent pas."
         if not check_password_validity(form_data.password):
-            return templates.TemplateResponse('register.html', {"request": request, 'errors': 'Le mot de passe doit contenir au moins 8 caractères dont au moins 1 lettre majuscule, 1 lettre minuscule, 1 chiffre et 1 caractère spécial.', 'form_data': form_data})
+            register_errors["password_not_fit"] = 'Le mot de passe doit contenir au moins 8 caractères dont au moins 1 lettre majuscule, 1 lettre minuscule, 1 chiffre et 1 caractère spécial.'
+        for value in register_errors.values():
+            if value != None:
+                return templates.TemplateResponse('register.html', {"request": request, 'errors': register_errors, 'form_data': form_data})
         query="INSERT INTO users VALUES(?, ?, ?, ?, 'MEMBER')"
         pw = generate_password_hash(password=form_data.password, method="pbkdf2:sha256", salt_length=8)
         db_cursor.execute(query, (None, form_data.username, form_data.email.lower(), pw,))
